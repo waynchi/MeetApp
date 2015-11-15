673f9199fd10538e364d883b178bf1fd8325c565
Benny Lau <bennycwlau@gmail.com>
2015-11-15 01:30:52 -0800

    next set of small changes

---
+++ meetUp/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  meetUp
+//
+//  Created by Apple on 11/15/15.
+//  Copyright © 2015 Wayne. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
